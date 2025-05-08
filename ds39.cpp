e5e8862e6881e849553a4220236bcf2241e737e7
Navin Songara <navinsongara1@gmail.com>
2025-05-08 23:26:57 +0530

    Add files via upload
    sas

---
+++ ds39.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverseArray(vector<int>& arr) {
+    int start = 0, end = arr.size() - 1;
+    while(start < end) {
+        swap(arr[start++], arr[end--]);
+    }
+}
+
+int main() {
+    vector<int> arr = {1, 2, 3, 4, 5};
+    reverseArray(arr);
+    for(int x : arr) cout << x << " ";
+}
