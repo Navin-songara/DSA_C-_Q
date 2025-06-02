375f6ca2e0a77ef0a378be0ecbef7f7a2c6a719d
Navin Songara <navinsongara1@gmail.com>
2025-06-02 20:35:49 +0530

    Add files via upload
    Burst Balloons (DP – Interval DP)

---
+++ dsa86.js
@@ -0,0 +1,22 @@
+function maxCoins(nums) {
+  nums = [1, ...nums, 1];
+  const n = nums.length;
+  const dp = Array.from({ length: n }, () => Array(n).fill(0));
+
+  for (let len = 2; len < n; len++) {
+    for (let left = 0; left < n - len; left++) {
+      let right = left + len;
+      for (let i = left + 1; i < right; i++) {
+        dp[left][right] = Math.max(
+          dp[left][right],
+          nums[left] * nums[i] * nums[right] + dp[left][i] + dp[i][right]
+        );
+      }
+    }
+  }
+
+  return dp[0][n - 1];
+}
+
+// Test
+console.log(maxCoins([3,1,5,8])); // Output: 167
